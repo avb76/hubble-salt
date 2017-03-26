52665c3407b14b3abcbe2e140cf094314b51c927
Gabriel Miu <miu@adobe.com>
2017-03-26 10:48:02 +0300

    New cve scan module

--- hubblestack_nova/cve_scan_v3.py
+++ hubblestack_nova/cve_scan_v3.py
@@ -1,25 +1,29 @@
 import requests
+import platform
+import sys
 
 
 def __virtual__():
-    return True
+    return not sys.platform.startswith('win')
 
 
-def _get_pkgs_list():
-    pkgs = __salt__['pkg.list_pkgs']()
-    return ['{0}-{1}'.format(pkg, pkgs[pkg]) for pkg in pkgs]
+def _get_local_packages():
+    local_packages = __salt__['pkg.list_pkgs']()
+    return ['{0}-{1}'.format(pkg, local_packages[pkg]) for pkg in local_packages]
 
 
-def _vulners_query(pkg_list, url='https://vulners.com/api/v3/audit/audit/'):
+def _vulners_query(packages, url='https://vulners.com/api/v3/audit/audit/'):
+    (os, version, _) = platform.dist()
+
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
     }
 
     data = {
-        "os": "centos",
-        "package": pkg_list,
-        "version": "7"
+        "os": os,
+        "package": packages,
+        "version": version
     }
 
     try:
@@ -34,5 +38,15 @@ def _vulners_query(pkg_list, url='https://vulners.com/api/v3/audit/audit/'):
         }
 
 
-def audit():
-    return _vulners_query(_get_pkgs_list())
+def _process_vulners(vulners):
+    return vulners
+
+
+def audit(data_list, tags, debug=False):
+    ret = {'Success': [], 'Failure': [], 'Controlled': []}
+
+    # get the vulnerable packages on the system
+    vulners = _vulners_query(_get_local_packages()).get('data').get('packages')
+    ret['Failure'] = _process_vulners(vulners)
+
+    return ret
